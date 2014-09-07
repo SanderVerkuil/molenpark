1211c02f748db5352b73519ca0bd29028a01fa64
Sander Verkuil <saverkuil@gmail.com>
2014-09-07 23:37:33 +0200

    Added some files, which somehow were not included... :/

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit85a4d9046d0ab7a33b116f38e5ec9ae7::getLoader();
+return ComposerAutoloaderInit132d51baf4e50b2023d1e2cd91144091::getLoader();
