553caf10eca6029cfd1cc76cb5b496a60aa95aa4
Sander Verkuil <saverkuil@gmail.com>
2014-11-01 00:51:45 +0100

    Wee, there is space between the rows. :3 THis was fixed by using a panel. :D

--- app/views/songs/index.blade.php
+++ app/views/songs/index.blade.php
@@ -1,42 +1,46 @@
 @extends('layouts/master')
 
 @section('content')
+
 <div class="row">
-  <div class="col-sm-12">
+  <div class="col-sm-12 column">
     <form role="form" method="get" id="search">
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="form-group">
-            <label for="artiest">Artiest</label>
-            <div class="input-group">
-              <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span></div>
-              <input type="text" name="artiest" id="artiest" placeholder="Artiest" class="form-control song-search" />
+      <div class="panel">
+        <div class="row">
+          <div class="col-sm-6 column">
+            <div class="form-group">
+              <label for="artiest">Artiest</label>
+              <div class="input-group">
+                <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span></div>
+                <input type="text" name="artiest" id="artiest" placeholder="Artiest" class="form-control song-search" />
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 column">
+            <div class="form-group">
+              <label for="title">Titel</label>
+              <div class="input-group">
+                <div class="input-group-addon"><span class="glyphicon glyphicon-music"></span></div>
+                <input type="text" name="titel" id="title" placeholder="Titel" class="form-control song-search" />
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-sm-6">
-          <div class="form-group">
-            <label for="title">Titel</label>
-            <div class="input-group">
-              <div class="input-group-addon"><span class="glyphicon glyphicon-music"></span></div>
-              <input type="text" name="titel" id="title" placeholder="Titel" class="form-control song-search" />
-            </div>
+        <div class="row">
+          <div class="col-sm-12 column">
+            <button type="button" form="search" class="btn btn-primary btn-block">Zoeken</button>
           </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-12">
-          <button type="button" form="search" class="btn btn-primary btn-block">Zoeken</button>
         </div>
       </div>
     </form>
   </div>
 </div>
-<div class="row" style="margin-top: 10px">
-  <div class="col-sm-12">
+<div class="row">
+  <div class="col-sm-12 column">
     <div id="overview">
     </div>
   </div>
 </div>
+
 @stop
 
